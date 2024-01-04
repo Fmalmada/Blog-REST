b0343f5f7b2b746227fc704b0cb3dfb5bfa2d877
fmalmada <fmalmada@fi.uba.ar>
2024-01-03 22:24:44 -0300

    Http basic

--- src/main/java/com/blog/config/SecurityConfig.java
+++ src/main/java/com/blog/config/SecurityConfig.java
@@ -2,16 +2,34 @@ package com.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable());
-        return http.build();
+        http.csrf((csrf) -> csrf.disable())
+
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(authorize -> {authorize
+                .requestMatchers("/h2-console/**").permitAll()            
+				.anyRequest().authenticated();
+            }
+			)
+        ;
+
+        /*http.authorizeHttpRequests(authorize -> {authorize
+                .requestMatchers("/h2-console/**").permitAll()            
+				.anyRequest().authenticated();
+            }
+			);*/
+
+            return http.build();
     }
     
 }
